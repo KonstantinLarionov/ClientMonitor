e746fa9ecf38d5174920617ceac42c2a38422263
Константин Ларионов <kostya12277@yandex.ru>
2021-10-18 16:58:33 +0500

    integrated tests

---
+++ ClientMonitor.Test/IntegratedTests/CloudTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClientMonitor.Test.IntegratedTests
+{
+    public class CloudTests
+    {
+        [Fact]
+        public void Test1()
+        { 
+        
+        }
+    }
+}
