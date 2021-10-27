77bd7043082b203cba761f49f3ee9d617103d359
Мария Гетьман <deprivedofwonders@gmail.com>
2021-10-27 13:20:23 +0500

    ~~

---
+++ ClientMonitor.Application/Domanes/Objects/DataServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientMonitor.Application.Domanes.Objects
+{
+    public class DataServer
+    {
+        public string IpServer { get; set; }
+        public int PortServer { get; set; }
+        public string GetInfo()
+        {
+            return ($"Ip сервера: {IpServer}  Номер порта: {PortServer}");
+        }
+    }
+}
