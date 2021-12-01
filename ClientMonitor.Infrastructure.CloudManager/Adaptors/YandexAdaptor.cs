bdb0209b3db71b954bca8703ee992eaf85bd35fb
Мария Гетьман <deprivedofwonders@gmail.com>
2021-12-01 17:26:23 +0500

    min (YandexAdaptor) : http request

--- ClientMonitor.Infrastructure.CloudManager/Adaptors/YandexAdaptor.cs
+++ ClientMonitor.Infrastructure.CloudManager/Adaptors/YandexAdaptor.cs
@@ -20,6 +20,7 @@ namespace ClientMonitor.Infrastructure.CloudManager.Adaptors
     {
         readonly CloudOptions _сloudOptions;
         readonly IMapper _мapper;
+        private readonly string ContentType = "application/binary";
 
         public YandexAdaptor(CloudOptions cloudOptions, IMapper mapper)
         {
@@ -56,32 +57,48 @@ namespace ClientMonitor.Infrastructure.CloudManager.Adaptors
         /// <returns></returns>
         public async Task UploadFiles(UploadedFilesInfo uploadedFilesInfo)
         {
-            #region [Беспощадная попытка]
-            //HttpClient client = new HttpClient();
-            //HttpRequestMessage request = new HttpRequestMessage();
-            //request.RequestUri = new Uri("https://webdav.yandex.ru");
-            //request.Method = HttpMethod.Put;
+            //#region [Запрос на облако]
+            // Заводим клиента
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_сloudOptions.BaseAddress);
+            // Берем файл в поток
+            //StreamContent sr = new StreamContent(new FileStream(uploadedFilesInfo.Path+@"\\"+ uploadedFilesInfo.Name, FileMode.Open));
+            StreamContent sr = new StreamContent(new FileStream(@"C:\Test\Test1\Озон-ПГ-Склад_2021_11_29_13_7_24 — копия (2).mp4", FileMode.Open));
+            // Заводим запрос
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, "Озон-ПГ-Склад_2021_11_29_13_7_24 — копия (2).mp4");
+            // передаем файл в запрос
+            request.Content = sr;
+            // Указываем заголовки запроса
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue(ContentType);
+            request.Headers.Add("Authorization", $"Basic {BasicAuth}");
+            request.Headers.Add("Transfer-Encoding", "chunked");
 
-            ////StreamReader sr = new StreamReader("test.txt", System.Text.Encoding.Default);
-            ////FileStream sr = new FileStream("test.txt", FileMode.Create, System.IO.FileAccess.Write);
-            ////StreamContent sr = new StreamContent(new FileStream("test.txt", FileMode.Open));
-
-            //StreamContent sr = new StreamContent(new FileStream(@"C:\box.png", FileMode.Open));
-            //request.Content = sr;
-            //request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/binary");
-            //request.Headers.Add("Authorization: OAuth", "AQAAAAA0xXEYAAdv3jbmZQ52CEQyv4Hw3ibzF_o");
-            //var response = await client.SendAsync(request);
-            //Console.WriteLine(response);
-            #endregion
-
-            var rootFolderData = await GetFilesAndFoldersAsync();
-            var conect = new DiskHttpApi(_сloudOptions.Token);
-            var link = await conect.Files.GetUploadLinkAsync(_сloudOptions.Path + uploadedFilesInfo.FolderName + "/" + uploadedFilesInfo.Name, overwrite: false);
-
-            using (var fs = File.OpenRead(uploadedFilesInfo.Path + "/" + uploadedFilesInfo.Name))
+            // Отправляем
+            try
             {
-                await conect.Files.UploadAsync(link, fs);
+                var response = await client.SendAsync(request);
+                Console.WriteLine(response);
             }
+            catch (Exception e) 
+            {
+            
+            }
+            //if ((int)response.StatusCode == 201)
+            //{
+
+            //    //чето написать, можно в логи добавить
+            //}
+
+            //#endregion
+
+            //var rootFolderData = await GetFilesAndFoldersAsync();
+            //var conect = new DiskHttpApi(_сloudOptions.Token);
+            //var link = await conect.Files.GetUploadLinkAsync(_сloudOptions.Path + uploadedFilesInfo.FolderName + "/" + uploadedFilesInfo.Name, overwrite: false);
+
+            //using (var fs = File.OpenRead(uploadedFilesInfo.Path + "/" + uploadedFilesInfo.Name))
+            //{
+            //    await conect.Files.UploadAsync(link, fs);
+            //}
         }
 
         /// <summary>
@@ -96,6 +113,28 @@ namespace ClientMonitor.Infrastructure.CloudManager.Adaptors
             var conect = new DiskHttpApi(_сloudOptions.Token);
             await conect.Files.DownloadFileAsync(path: Path.Combine(cloudpath, name), Path.Combine(downloadpath, name));
             return true;
+        }
+
+        /// <summary>
+        /// Создание строки для авторизации
+        /// </summary>
+        private string BasicAuth
+        {
+            get
+            {
+                return Base64Encode($"{_сloudOptions.Login}:{_сloudOptions.Password}");
+            }
+        }
+
+        /// <summary>
+        /// Хелперс для кодинга строки
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        private string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
 
     }
