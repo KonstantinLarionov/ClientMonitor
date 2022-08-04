e5eb99b81b5568af94b01b1084098dd4d5ff4af2
Maria <Maria@DESKTOP-TELCHD7>
2022-08-04 08:02:00 +0500

    ~

--- ClientMonitor.Application/Handler/VideoControlHandler.cs
+++ ClientMonitor.Application/Handler/VideoControlHandler.cs
@@ -49,7 +49,7 @@ namespace ClientMonitor.Application.Handler
             new ControlVideoInfo
             {
                 Name="Озон-ПГ-Выдача",
-                PathStream=new Uri("rtsp://PoligonnayaZal:123456@192.168.1.9:554/stream1"),
+                PathStream=new Uri("rtsp://PoligonnayaZal:123456@192.168.1.8:554/stream1"),
                 PathDownload=@"C:\Users\Big Lolipop\Desktop\ЗаписиКамер2\Ozon\Выдача"
             },
             new ControlVideoInfo
