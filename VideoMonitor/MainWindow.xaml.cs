342e54f9199e2e19cc08496cd1195f113958999d
Константин Ларионов <kostya12277@yandex.ru>
2023-01-19 21:22:54 +0500

    ~~

---
+++ VideoMonitor/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoMonitor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void save_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void onOff_tgl_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void onOff_tgl_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void path_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ftp_chk_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ftp_chk_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
